cf2a01c0d496c7f97f7f8dd965af48b3e16669d8
Jordan <jordan@infima.io>
2025-04-13 09:56:57 -0700

    add constants and main notebook

---
+++ constants.py
@@ -0,0 +1,25 @@
+# Dictionary to be used for converting amino acid abbreviations
+AA_3TOL = {
+    "CYS": "C",
+    "ASP": "D",
+    "SER": "S",
+    "GLN": "Q",
+    "LYS": "K",
+    "ILE": "I",
+    "PRO": "P",
+    "THR": "T",
+    "PHE": "F",
+    "ASN": "N",
+    "GLY": "G",
+    "HIS": "H",
+    "LEU": "L",
+    "ARG": "R",
+    "TRP": "W",
+    "ALA": "A",
+    "VAL": "V",
+    "GLU": "E",
+    "TYR": "Y",
+    "MET": "M",
+}
+
+AA = 'CDSQKIPTFNGHLRWAVEYM'
